d39ce2430d5ff35d2883a939497fb5bec31390a9
saksham <sakshamb2113@gmail.com>
2020-10-16 02:42:41 +0530

    added check for invalid input

--- sqrt.c
+++ sqrt.c
@@ -14,6 +14,12 @@ int main(int argc, char* argv[]) {
 	}
 
 	int input = atoi(argv[1]);
+	
+	if(input<0){
+		printf("Invalid number: Please input non negative numbers");
+		exit(-1);
+	}
+
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting.");
 	return(0);
